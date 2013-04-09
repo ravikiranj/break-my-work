48d9657014047e9cb9a2cc698dc9f929ba372c92
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2013-04-09 13:51:57 -0400

    Added enable/disable of short breaks ui

--- bmw/BreakMyWork.py
+++ bmw/BreakMyWork.py
@@ -23,6 +23,9 @@ class UI:
             self.noteBook.append_page(tabGrid, tabLabel)
         
     def buildTimerTabUI(self, tabGrid):
+        self.buildShortBreakUI(tabGrid)
+
+    def buildShortBreakUI(self, tabGrid):
         #short break label
         self.shortBreakLabel = Gtk.Label('Short Breaks')
         self.shortBreakLabel.modify_font(self.getFont("heading"))
@@ -30,45 +33,90 @@ class UI:
         self.setMargin(self.shortBreakLabel, "10 10 10 10");
         tabGrid.add(self.shortBreakLabel)
         
+        #short break enable/disable CheckBox
+        self.shortBreakEnableBtn = Gtk.CheckButton("Enable Short Breaks")
+        self.shortBreakEnableBtn.set_active(True)
+        self.shortBreakEnableBtn.connect("toggled", self.toggleShortBreaks)
+        self.shortBreakEnableBtn.set_margin_left(10)
+        tabGrid.attach_next_to(self.shortBreakEnableBtn, self.shortBreakLabel, Gtk.PositionType.BOTTOM, 1, 1);
+        
+        #short break vBox to hold timer choices
+        self.shortTimerChoiceBox = Gtk.Box()
+        self.shortTimerChoiceBox.set_orientation(Gtk.Orientation.VERTICAL)
+        self.setMargin(self.shortTimerChoiceBox, "10 0 10 30")
+        tabGrid.attach_next_to(self.shortTimerChoiceBox, self.shortBreakEnableBtn, Gtk.PositionType.BOTTOM, 1, 1)
+        
+        #short break duration hbox
+        self.shortBreakDurationBox = Gtk.Box()
+        self.shortBreakDurationBox.set_orientation(Gtk.Orientation.HORIZONTAL)
+        self.setMargin(self.shortBreakDurationBox, "0 0 5 0")
+        self.shortTimerChoiceBox.pack_start(self.shortBreakDurationBox, True, True, 0)
+        
+        #short break Duration label        
+        shortBreakDurationLabel = Gtk.Label("Duration")
+        self.shortBreakDurationBox.pack_start(shortBreakDurationLabel, True, True, 0)
+        
+        #value, lower, upper, step_inc, page_inc, page_size
+        self.shortBreakDurationAdjustment = Gtk.Adjustment(2, 1, 10, 1, 2, 0)
+        #short break custom mins spinner button
+        self.shortBreakDurationBtn = Gtk.SpinButton()
+        self.shortBreakDurationBtn.set_adjustment(self.shortBreakDurationAdjustment)
+        self.shortBreakDurationBtn.set_numeric(True)
+        self.shortBreakDurationBtn.set_digits(0)
+        self.shortBreakDurationBtn.set_update_policy(Gtk.SpinButtonUpdatePolicy.IF_VALID)
+        self.shortBreakDurationBtn.set_sensitive(True)
+        self.shortBreakDurationBtn.set_max_length(2)
+        self.shortBreakDurationBtn.set_margin_left(5)
+        self.shortBreakDurationAdjustment.connect("value_changed", self.updateShortBreakDuration)
+        self.shortBreakDurationBox.pack_start(self.shortBreakDurationBtn, True, True, 0)
+        
+        #short break Duration 'minutes' label        
+        shortBreakDurationMinsLabel = Gtk.Label("minutes")
+        shortBreakDurationMinsLabel.set_margin_left(2)
+        self.shortBreakDurationBox.pack_start(shortBreakDurationMinsLabel, True, True, 0)
         #short break 15 mins
         self.shortFifteenStr = "Once every 15 mins"
         self.shortFifteenBtn = Gtk.RadioButton.new_with_label_from_widget(None, self.shortFifteenStr);
-        self.shortFifteenBtn.connect("toggled", self.on_button_toggled, "short15");
-        tabGrid.attach_next_to(self.shortFifteenBtn, self.shortBreakLabel, Gtk.PositionType.BOTTOM, 1, 1);
+        self.shortFifteenBtn.connect("toggled", self.changeShortBreakTimer, "short15");
+        self.shortTimerChoiceBox.pack_start(self.shortFifteenBtn, True, True, 0)
         
         #short break 30 mins
         self.shortThirtyStr = "Once every 30 mins"
         self.shortThirtyBtn = Gtk.RadioButton.new_with_label_from_widget(self.shortFifteenBtn, self.shortThirtyStr);
-        self.shortThirtyBtn.connect("toggled", self.on_button_toggled, "short30");
-        tabGrid.attach_next_to(self.shortThirtyBtn, self.shortFifteenBtn, Gtk.PositionType.BOTTOM, 1, 1);
+        self.shortThirtyBtn.connect("toggled", self.changeShortBreakTimer, "short30");
+        self.shortTimerChoiceBox.pack_start(self.shortThirtyBtn, True, True, 0)
         
         #short break custom mins radio button
         self.shortCustomBtn = Gtk.RadioButton.new_with_label_from_widget(self.shortFifteenBtn, "Custom");
-        self.shortCustomBtn.connect("toggled", self.on_button_toggled, "shortCustom");
-        shortCustomHBox = Gtk.Box()
-        shortCustomHBox.pack_start(self.shortCustomBtn, True, True, 0)
+        self.shortCustomBtn.connect("toggled", self.changeShortBreakTimer, "shortCustom");
+        
+        #short break HBOX to hold custom mins data
+        self.shortCustomHBox = Gtk.Box()
+        self.shortCustomHBox.set_orientation(Gtk.Orientation.HORIZONTAL)
+        self.shortTimerChoiceBox.pack_start(self.shortCustomHBox, True, True, 0)
+        self.shortCustomHBox.pack_start(self.shortCustomBtn, True, True, 0)
         
         #value, lower, upper, step_inc, page_inc, page_size
-        self.shortBreakAdjustment = Gtk.Adjustment(60, 1, 999, 1, 10, 0)
+        self.shortBreakCustomMinsAdjustment = Gtk.Adjustment(60, 1, 999, 1, 10, 0)
         #short break custom mins spinner button
-        self.shortBreakSpinBtn = Gtk.SpinButton()
-        self.shortBreakSpinBtn.set_adjustment(self.shortBreakAdjustment)
-        self.shortBreakSpinBtn.set_numeric(True)
-        self.shortBreakSpinBtn.set_digits(0)
-        self.shortBreakSpinBtn.set_update_policy(Gtk.SpinButtonUpdatePolicy.IF_VALID)
-        self.shortBreakSpinBtn.set_margin_left(5)
-        self.shortBreakSpinBtn.set_sensitive(False)
-        self.shortBreakSpinBtn.set_wrap(True)
-        self.shortBreakAdjustment.connect("value_changed", self.updateCustomShortBreak)
-        shortCustomHBox.pack_start(self.shortBreakSpinBtn, True, True, 0)
+        self.shortBreakCustomMinsBtn = Gtk.SpinButton()
+        self.shortBreakCustomMinsBtn.set_adjustment(self.shortBreakCustomMinsAdjustment)
+        self.shortBreakCustomMinsBtn.set_numeric(True)
+        self.shortBreakCustomMinsBtn.set_digits(0)
+        self.shortBreakCustomMinsBtn.set_update_policy(Gtk.SpinButtonUpdatePolicy.IF_VALID)
+        self.shortBreakCustomMinsBtn.set_margin_left(5)
+        self.shortBreakCustomMinsBtn.set_max_length(3)
+        self.shortBreakCustomMinsBtn.set_sensitive(False)
+        self.shortBreakCustomMinsBtn.set_wrap(True)
+        self.shortBreakCustomMinsAdjustment.connect("value_changed", self.updateCustomShortBreak)
+        self.shortCustomHBox.pack_start(self.shortBreakCustomMinsBtn, True, True, 0)
         
         #short break custom mins label
         shortCustomLabel = Gtk.Label("minutes")
         shortCustomLabel.set_margin_left(5)
-        shortCustomHBox.pack_start(shortCustomLabel, True, True, 0)
+        self.shortCustomHBox.pack_start(shortCustomLabel, True, True, 0)
         
-        #Add HBox to the grid
-        tabGrid.attach_next_to(shortCustomHBox, self.shortThirtyBtn, Gtk.PositionType.BOTTOM, 1, 1);
+
         
     def getFont(self, type):
         if(type == "heading"):
@@ -92,26 +140,29 @@ class UI:
         else:
             raise ValueError("button does not have a label")
             
-    def shortBreakCustomEntryChanged(self, btn, name):
-        if(name == "custom"):
-            self.updateShortTimer(name)
-            
     def updateShortTimer(self, name):
         if(name == "custom"):
-            currVal = self.shortBreakSpinBtn.get_value_as_int()
+            currVal = self.shortBreakCustomMinsBtn.get_value_as_int()
             print "Activate Timer Once every ", currVal, " mins (custom)"
         elif(name == "short15"):
             print "Activate Timer Once every 15 mins"
         elif(name == "short30"):
             print "Activate Timer Once every 30 mins"
             
+    def toggleShortBreaks(self, btn):        
+        if(btn.get_active()):
+            #enable short breaks
+            self.shortTimerChoiceBox.set_sensitive(True)
+        else:
+            #disable short breaks
+            self.shortTimerChoiceBox.set_sensitive(False)
             
     def addLabelToGrid(self, tabGrid, labelName):
         containerLabel = Gtk.Label(labelName+" is inside tab")
         containerLabel.show()
         tabGrid.add(containerLabel)
         
-    def on_button_toggled(self, btn, name):
+    def changeShortBreakTimer(self, btn, name):
         state = 0
         if(btn.get_active()):
             state = 1
@@ -123,13 +174,17 @@ class UI:
                 self.updateShortTimer("short30")
         elif(name == "shortCustom"):
             if(state == 1):
-                self.shortBreakSpinBtn.set_sensitive(True)
+                self.shortBreakCustomMinsBtn.set_sensitive(True)
                 self.updateShortTimer("custom")
             else:
-                self.shortBreakSpinBtn.set_sensitive(False)
+                self.shortBreakCustomMinsBtn.set_sensitive(False)
         
     def updateCustomShortBreak(self, btn):
         self.updateShortTimer("custom")
+        
+    def updateShortBreakDuration(self, btn):
+        currVal = self.shortBreakDurationBtn.get_value_as_int()
+        print "Short Break Duration =  ", currVal, " mins"
             
     def numbify(self, widget):
         def filter_numbers(entry, *args):
